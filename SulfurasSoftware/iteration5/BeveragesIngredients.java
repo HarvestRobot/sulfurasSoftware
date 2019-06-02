4ecd48265668510f08bf9811289380fcda0f1c3d
Varian Wrynn <lucia@DESKTOP-END55K0>
2019-06-02 22:30:33 +0200

    Today is 06/06/2019. I am Varian Wrynn. This is iteration 5.

---
+++ SulfurasSoftware/iteration5/BeveragesIngredients.java
@@ -0,0 +1,13 @@
+public class BeveragesIngredients {
+
+	public string[][] ingredients;
+
+	public string[][] getIngredients() {
+		return this.ingredients;
+	}
+
+	public void setIngredients(string[][] ingredients) {
+		this.ingredients = ingredients;
+	}
+
+}
