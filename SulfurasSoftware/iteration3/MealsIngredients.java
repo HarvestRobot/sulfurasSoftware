5b11c42a40a12d0d7ed932bb4a239288e3106344
Rick Astley <lucia@DESKTOP-END55K0>
2019-06-02 22:18:15 +0200

    Today is 29/05/19. I am Rick Astley. This is iteration3.

---
+++ SulfurasSoftware/iteration3/MealsIngredients.java
@@ -0,0 +1,16 @@
+public class MealsIngredients {
+
+	/**
+	 * array of ingredients with the number of each
+	 */
+	public string[][] ingredients;
+
+	public string[][] getIngredients() {
+		return this.ingredients;
+	}
+
+	public void setIngredients(string[][] ingredients) {
+		this.ingredients = ingredients;
+	}
+
+}
---
+++ SulfurasSoftware/iteration3/MealsManagement.java
@@ -0,0 +1,46 @@
+public class MealsManagement {
+
+	/**
+	 * this method notifies the waiter that needs to come to pick up a meal
+	 */
+	public void notifyWaiter() {
+		// TODO - implement MealsManagement.notifyWaiter
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * this method updates the ingredients on stock after making a meal calling updateIngredients() and notifies the waiter calling notifyWaiter()
+	 * @param meal
+	 * @param tableID
+	 */
+	public void finishedMeal(Meal meal, Table tableID) {
+		// TODO - implement MealsManagement.finishedMeal
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * updates the ingredientes avaiable after making the selected meal. After updating, this method calls enoughIngredients() to check if more ingredients need to be bought.
+	 * @param meal
+	 */
+	public void updateIngredients(Meal meal) {
+		// TODO - implement MealsManagement.updateIngredients
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * this method alert whether certain ingredient goes below a range
+	 */
+	public void enoughIngredients() {
+		// TODO - implement MealsManagement.enoughIngredients
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * this method will calculate the times to later generate statistics
+	 */
+	public void getTimes() {
+		// TODO - implement MealsManagement.getTimes
+		throw new UnsupportedOperationException();
+	}
+
+}
